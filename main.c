4e66e21facbd0a64b232e1bf480641675d9d9366
nguyenqucong <congnqd05578@fpt.edu.vn>
2017-08-31 21:14:29 +0700

    them ham nhan va chia.

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
-main() {
-	printf ("%i\n",plus (8,9));
-	printf ("%i",minus (8,9));
+main(){
+	printf("%i",plus(8,9));
+	printf("%i",minus(8,9));
 }
-//cong hai so voi tham so dau vao
-//la a ,b la hai so nguyen
-int plus (int a, int b){
-	return a+b;
-}
-
-int minus (int a, int b){
-	return a - b
-}
+ //cong hai so voi tham so dau vao.
+ //a va b la hai so nguyen.
+ int plus (int a, int b){
+ 	return a+b;
+ }
+ int minus (int a, int b){
+ 	return a-b;
+ }
+ 
+ int Multiplication (int a, int b){
+ 	return a*b;
+ }
+ 
+ int divide (int a, int b){
+ 	return a/b;
+ }
