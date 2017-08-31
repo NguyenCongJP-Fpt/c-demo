d6ec5a1bee2c7a6d1ed765e97e06f43dec5a10ea
nguyenqucong <congnqd05578@fpt.edu.vn>
2017-08-31 20:36:12 +0700

    them ham cong tru

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 main() {
-	printf ("%i",plus (8,9));
+	printf ("%i\n",plus (8,9));
+	printf ("%i",minus (8,9));
 }
 //cong hai so voi tham so dau vao
 //la a ,b la hai so nguyen
 int plus (int a, int b){
 	return a+b;
+}
+
+int minus (int a, int b){
+	return a - b
 }
