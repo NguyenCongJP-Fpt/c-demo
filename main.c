c7de03629a95b6dd3042a4c3942d63a47a21e474
NguyenCongJP-Fpt <congnqd00578@fpt.edu.vn>
2017-09-05 15:11:49 +0700

    message

--- main.c
+++ main.c
@@ -1,21 +1,47 @@
 #include <stdio.h>
 main(){
-	printf("%i",plus(8,9));
-	printf("%i",minus(8,9));
+	int a;
+	int b;
+	int c;
+	printf("Please enter number a = ");
+	scanf("%d",&a);
+	printf("Please enter number b = ");
+	scanf("%d",&b);
+	printf("Please choice:\n (1:+ (plus), 2: - (minus), 3: * (multiplication), 4: / (division))=> ");
+	scanf("%d",&c);
+	if(c==1){
+	printf("Your result is: %d\n",a+b);
+	printf("Omedetou gozaimasu");
 }
+	if(c==2){
+	printf("Your result is: %d\n",a-b);
+	printf("Omedetou gozaimasu");
+}
+	if(c==3){
+	printf("Your result is: %d\n",a*b);
+	printf("Omedetou gozaimasu");
+}	
+	if(c==4){
+	printf("Your result is: %d\n",a/b);
+	printf("Omedetou gozaimasu");
+}
+ 
  //cong hai so voi tham so dau vao.
  //a va b la hai so nguyen.
  int plus (int a, int b){
  	return a+b;
  }
+ //tru 2 so.
  int minus (int a, int b){
  	return a-b;
  }
- 
- int Multiplication (int a, int b){
+  //nhan hai so.
+  int multiplication (int a, int b){
  	return a*b;
  }
- 
- int divide (int a, int b){
+  //chia hai so. 
+ int division (int a, int b){
  	return a/b;
- }
+}
+return 0;
+}
